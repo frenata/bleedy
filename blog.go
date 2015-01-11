c427a6f19733f2564ebb5cd78f36e39c87bbb231
Andrew Nichols <danweasel@gmail.com>
2015-01-11 19:16:15 +0700

    improved docs

--- blog.go
+++ blog.go
@@ -8,19 +8,24 @@ import (
 	"time"
 )
 
+// Blog holds the definitions of file directories/extensions for inputs, outputs, and templates, as well as the hashmap
+// for checking time of last modification of files in the input directory.
 type Blog struct {
 	input    files                //  read directory/ext
 	output   files                //  write directory/ext
-	template files                // template direcoty/ext
+	template files                // template directory/ext
 	hash     map[string]time.Time // hash map to check for updates
 }
 
+// small struct to make Blog look prettier, defines a location and type of file, as well as a default filename.
 type files struct {
 	dir string
 	ext string
 	def string
 }
 
+// NewBlog creates a new Blog object, populated with all the directories/extensions for input/ouput/templates.
+// It also allocates the hashmap for checking file modification times.
 func NewBlog(iDir, iExt, oDir, oExt, tDir, tExt string) *Blog {
 	b := &Blog{hash: make(map[string]time.Time)}
 	b.SetInput(iDir, iExt)
@@ -30,23 +35,25 @@ func NewBlog(iDir, iExt, oDir, oExt, tDir, tExt string) *Blog {
 	return b
 }
 
-// SetInput sets the input directory and file extension, this must be set when calling New, so not likely anyone will
-// use this unless changing directories on the fly is somehow required, included for completeness though
+// SetInput sets the directory and file extension for markeddown text files.
 func (b *Blog) SetInput(dir, ext string) {
 	b.input.dir = dir
 	b.input.ext = ext
 }
 
+// SetOutput sets the directory and file extension for generated html files.
 func (b *Blog) SetOutput(dir, ext string) {
 	b.output.dir = dir
 	b.output.ext = ext
 }
 
+// SetTemplate sets the directory and file extension for template files.
 func (b *Blog) SetTemplate(dir, ext string) {
 	b.template.dir = dir
 	b.template.ext = ext
 }
 
+// SetDefaultTemplate sets the filename for the default post template. This should be in the template directory.
 func (b *Blog) SetDefaultTemplate(def string) {
 	if def != "" {
 		b.template.def = def
@@ -55,22 +62,21 @@ func (b *Blog) SetDefaultTemplate(def string) {
 	}
 }
 
+// reads from the specified input file (markdown), creates a new Post, and returns it.
 func (b *Blog) readFile(file string, date time.Time) (*Post, error) {
 	content, err := ioutil.ReadFile(path.Join(b.input.dir, file) + b.input.ext)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
-	// TODO: call a parse function, read to struct?
-	//ioutil.WriteFile(path.Join(p.staticDir, file)+p.staticExt, output, 0666)
-	//fmt.Println(string(content))
+
 	p := NewPost(content, date)
-	//fmt.Println(p)
+	//fmt.Println(p) //testing
 	return p, nil
 }
 
+// formats and writes the content of a Post to the specified file
 func (b *Blog) writeFile(file string, p *Post) error {
-	//quick and dirty, replacing with templating
 	name := ""
 	if name = p.Template(); name == "" {
 		name = b.template.def
@@ -90,38 +96,42 @@ func (b *Blog) writeFile(file string, p *Post) error {
 	return nil
 }
 
-// Update checks the read directory for changes to files
+// Update checks the read directory for changes to files. If it detects changes (based on last-modified time),
+// it reads the input file and creates an output file of the same name.
+// Designed to be called continously in a loop.
 func (b *Blog) Update() {
+	// read all the files in the input directory
 	files, err := ioutil.ReadDir(b.input.dir)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// check each file
 	for _, f := range files {
 		n := f.Name()
 		switch {
-		case f.IsDir():
-			continue // TODO: descend into directory, auto-tag with name
-		case strings.HasSuffix(n, b.input.ext):
-			n := strings.TrimSuffix(n, b.input.ext)
-			if _, ok := b.hash[n]; ok {
-				fmt.Printf("%v was last updated %v ago\n", n, time.Since(b.hash[n]))
+		case f.IsDir(): // TODO: descend into directory, auto-tag with name
+			continue
+		case strings.HasSuffix(n, b.input.ext): // check the suffix
+			n := strings.TrimSuffix(n, b.input.ext) // remove the suffix
+			if _, ok := b.hash[n]; ok {             // is it already in the hashmap?
+				fmt.Printf("%v was last updated %v ago\n", n, time.Since(b.hash[n])) // TODO: log not print
 				if b.hash[n] == f.ModTime() {
-					continue
+					continue // file has not been modified since the last check, ignore it
 				}
 			}
-			b.hash[n] = f.ModTime()
-			fmt.Printf("Update %v\n", n)
-			p, err := b.readFile(n, f.ModTime())
+			b.hash[n] = f.ModTime()                 // store the last modified time
+			fmt.Printf("Update %v\n", n)            // TODO: log not print
+			post, err := b.readFile(n, f.ModTime()) // read the file, creating a post
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println(err) // TODO: log not print
 				return
 			}
-			//fmt.Println(p)a
-			err = b.writeFile(n, p)
+
+			err = b.writeFile(n, post) // write the post to a file
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println(err) // TODO: log not print
 				return
 			}
 		}
--- post.go
+++ post.go
@@ -1,12 +1,14 @@
 package blog
 
 import (
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/russross/blackfriday"
 )
 
+// TODO: These should either be part of the Post struct or live in a config file.
 const (
 	titlePre    string = "Title: "
 	authorPre   string = "Author: "
@@ -14,21 +16,22 @@ const (
 	datePre     string = "Date: "
 	templatePre string = "Template: "
 	dateFormat  string = "2 January 2006 @ 3:04pm"
-	//dateFormat string = "January 2, 2006"
-	//dateFormat string = time.RFC822Z
 )
 
+// Post holds the content of a post, parsed from a file, metadata and body content.
 type Post struct {
-	title      string
-	author     string
-	tag        string
-	body       string
-	template   string
-	date       time.Time
-	dateFormat string
-	//comments Comments
+	title    string
+	author   string
+	tag      string
+	body     string
+	template string
+	date     time.Time
+	//comments Comments // TODO: bool for disqus comments?
 }
 
+// NewPost takes a byte slice (from a markdowned text file), a date, and creates a new Post object.
+// The date should typically be the last modification time of the file, and will be overwritten
+// if a date is manually set in the Post metadata.
 func NewPost(raw []byte, date time.Time) *Post {
 	p := new(Post)
 	content := string(raw)
@@ -37,34 +40,10 @@ func NewPost(raw []byte, date time.Time) *Post {
 	if len(c) < 2 {
 		return nil
 	}
-	//m := c[0]
-	//b := []byte(c[1])
+
 	// TODO: does this need validation / error checking?
 	p.body = string(blackfriday.MarkdownCommon([]byte(c[1])))
-
 	meta := strings.Split(c[0], "\n")
-	/* if len(m) < 3 {
-		return nil
-	}
-	// TODO: should probably validate that these are tagged properly
-	// beter yet, iterate through everything in the meta section and assign iff those variables
-	// that validate
-	title := strings.TrimPrefix(m[0], titlePre)
-	author := strings.TrimPrefix(m[1], authorPre)
-	tag := strings.TrimPrefix(m[2], tagPre)
-	if len(m) > 3 {
-		// TODO validate this one too
-		s := strings.TrimPrefix(m[3], datePre)
-		s = strings.TrimSpace(s)
-		d, err := time.Parse(dateFormat, s)
-		if err == nil {
-			date = d
-		} else {
-			//fmt.Println(d)
-			//fmt.Println(err)
-		}
-	} */
-	p.dateFormat = dateFormat
 
 	// new version of meta yanking: just grab each line, check if it validates any Meta tags, and assign it properly
 	for _, m := range meta {
@@ -82,18 +61,19 @@ func NewPost(raw []byte, date time.Time) *Post {
 
 	}
 
-	//comments := nil //to implement
 	return p
 }
 
+// parses the string against the specified dateformat, if it validates, manually set the post date
 func (p *Post) setDate(s string) {
-	d, err := time.Parse(p.dateFormat, s)
+	d, err := time.Parse(dateFormat, s)
 	if err != nil {
 		return
 	}
 	p.date = d
 }
 
+// checks the string for valid metadata, as defined in the constant prefixes, and returns the data.
 func (p *Post) validateMeta(m, pre string) (ok bool, output string) {
 	if strings.HasPrefix(m, pre) {
 		ok, output = true, strings.TrimSpace(strings.TrimPrefix(m, pre))
@@ -104,6 +84,7 @@ func (p *Post) validateMeta(m, pre string) (ok bool, output string) {
 	return ok, output
 }
 
+// String prints the Post meta content and body.
 func (p *Post) String() string {
 	t := "Title: " + p.title + "\n"
 	a := "Author: " + p.author + "\n"
@@ -113,12 +94,15 @@ func (p *Post) String() string {
 	return t + a + g + d + "\n" + p.body
 }
 
+// Template returns the Post specific template. If one was not set in the metadata, this should return ""
 func (p *Post) Template() string {
 	return p.template
 }
 
+// Format takes a template file and creates a []byte representing an html document populated with the Post content,
+// ready for writing to a file.
 func (p *Post) Format(template string) ([]byte, error) {
-	//bytes := []byte(fmt.Sprint(p))
+	bytes := []byte(fmt.Sprint(p))
 
 	return bytes, nil
 }
